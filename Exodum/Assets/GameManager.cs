17f5a7f468e0b9ae051bc3ed8503cf6127545ebb
ncapolt <140573792+ncapolt@users.noreply.github.com>
2024-08-20 12:03:09 -0300

    commit?
    qa ver si

---
+++ Exodum/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGanager : MonoBehaviour
+{
+    public int vida;
+    public int puntaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Exodum/Assets/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    public AudioSource source;
+    public AudioSource sourceSFX;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this);
+
+        source = GetComponent<AudioSource>();
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        source.loop = false;
+        source.PlayOneShot(clip);
+        
+    }
+
+
+
+}
+
+
