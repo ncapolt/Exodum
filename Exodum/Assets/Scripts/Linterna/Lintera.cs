44e8c1433d65c809eb170de02b8d738ddf3a76f4
LOW TANNE NICOLÁS ELÍAS <140573792+ncapolt@users.noreply.github.com>
2024-09-05 15:52:23 -0300

    linterna funcionando y escena oscura

---
+++ Exodum/Assets/Scripts/Linterna/Lintera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lintera : MonoBehaviour
+{
+    public Light LuzLinterna;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Linterna"))
+        {
+            if(LuzLinterna.enabled == true)
+            {
+                LuzLinterna.enabled = false;
+            }
+            else if (LuzLinterna.enabled == false)
+            {
+                LuzLinterna.enabled = true;
+            }
+        }
+    }
+}
