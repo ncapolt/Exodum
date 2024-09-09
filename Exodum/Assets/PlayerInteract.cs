544e2d208a9d8ddeeaf143e9b97a40aaf11c5e32
ncapolt <nicolaslowtanne@gmail.com>
2024-09-08 22:09:54 -0300

    puzzle medios pt1

---
+++ Exodum/Assets/PlayerInteract.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    
+    
+    // Update is called once per frame
+   private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+        float interactRange = 2f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);   
+            foreach (Collider collider in colliderArray)
+        {
+            Debug.Log(collider);
+        }
+       }
+    }
+}
