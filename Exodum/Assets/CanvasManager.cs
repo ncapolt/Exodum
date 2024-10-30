857cfa496ff4520826a89c603606d6f98451b04a
ncapolt <140573792+ncapolt@users.noreply.github.com>
2024-10-30 14:24:15 -0300

    Menu anda perfecto, falta la UI

---
+++ Exodum/Assets/CanvasManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject canvasMenu;     // Canvas del Menú Principal
+    public GameObject canvasCreditos; // Canvas de Créditos
+
+    // Muestra el Canvas del Menú Principal y oculta el de Créditos
+    public void MostrarMenu()
+    {
+        canvasMenu.SetActive(true);
+        canvasCreditos.SetActive(false);
+    }
+
+    // Muestra el Canvas de Créditos y oculta el del Menú Principal
+    public void MostrarCreditos()
+    {
+        canvasMenu.SetActive(false);
+        canvasCreditos.SetActive(true);
+    }
+}
