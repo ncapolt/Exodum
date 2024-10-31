a9a17f2f9478630d7383a585b97dddcf14bd7c80
ncapolt <140573792+ncapolt@users.noreply.github.com>
2024-10-31 12:02:19 -0300

    GUARDIA ANDAAAA (THIAGO)

---
+++ Exodum/Assets/GuardiaScript.cs
@@ -0,0 +1,65 @@
+﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+public class GuardiaScript : MonoBehaviour
+{
+    public float DistanciaVision;
+    public NavMeshAgent guardia;
+    public Transform jugador;
+    public Animator anim;
+    public bool isChasing = false;
+    public LayerMask PLLayer;
+    public Vector3 destination;
+    public Transform RayPosi;
+    void Start()
+    {
+        if (guardia == null)
+            guardia = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        destination = guardia.destination;
+        RaycastHit hit;
+        Vector3 direccion = jugador.position - transform.position;
+
+        if (Physics.Raycast(RayPosi.position, RayPosi.forward, out hit, DistanciaVision, PLLayer, QueryTriggerInteraction.Collide))
+        {
+            Debug.Log(hit.transform);
+            if (hit.transform.gameObject.GetComponent<SC_FPSController>() != null)
+            {
+                Perseguir();
+            }
+        }
+    }
+
+    void Perseguir()
+    {
+        if (!isChasing)
+        {
+            isChasing = true;
+            guardia.destination = jugador.position;
+        }
+        else
+        {
+            isChasing =false;
+        }
+
+        Debug.Log("perseguir");
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (jugador != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(RayPosi.position, RayPosi.position + RayPosi.forward * 5);
+        }
+    }
+}
---
+++ Exodum/Assets/gizmo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gizmo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private void OnDrawGizmos()
+    //{
+        
+    //        Gizmos.color = Color.green;
+    //        Gizmos.DrawLine(transform.position, transform.up * 2f);
+    //}
+}
