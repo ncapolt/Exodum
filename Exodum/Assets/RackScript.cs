0ebfcfb2b667fcfa74f869173642afe809b0f166
ncapolt <140573792+ncapolt@users.noreply.github.com>
2024-11-04 09:02:49 -0300

    ANDA EL TEXTO puzzle completo basicamente

--- Exodum/Assets/RackScript.cs
+++ Exodum/Assets/RackScript.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RackScript : MonoBehaviour
 {
+    public Text loretext;
     public GameObject martillo;
-    public puzzle_tic puzzleScript; // Referencia al script del puzzle
+    public puzzle_tic puzzleScript;
+
 
     public void Interact()
     {
-        if (puzzleScript.puzzleCompletado) // Verifica si el puzzle está completado
+        if (puzzleScript.puzzleCompletado)
         {
-            martillo.SetActive(true); // Activa el GameObject martillo
+            martillo.SetActive(true);
         }
-       
+        else
+        {
+            loretext.text = "Debo completar el Ahorcado para abrir el rack";
+            StartCoroutine(ClearMessageAfterDelay(3f));
+        }
+
+        IEnumerator ClearMessageAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay); // Esperar los 3 segundos
+            loretext.text = "";
+
+        }
     }
 }
